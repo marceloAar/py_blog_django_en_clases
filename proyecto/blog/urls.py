678127c868ebf25bb1a8fe1354b712ace690acba
MarceloAar <marcelo_aar@hotmail.com>
2024-10-23 22:13:38 -0300

    se agrega los articulos del blog

--- proyecto/blog/urls.py
+++ proyecto/blog/urls.py
@@ -5,4 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index' ),
+    path('articulos/', views.articulos_list, name='articulos_list')
 ]
--- proyecto/blog/views.py
+++ proyecto/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Articulo
 
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def articulos_list(request):
+    #obtiene todos los articulos de BBDD
+    articulos = Articulo.objects.all()
+    return render(request, 'blog/articulos_list.html', {'articulos': articulos})
+ 
 
